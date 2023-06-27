018c7e21153a6dd5dd1704e622d9c6042b030475
inderjeet18th <inderjeet@18thdigitech.net>
2023-06-27 20:40:37 +0530

    [Feature] Base module start [wip]

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eighteentech_Base module
+
+The Eighteentech_Base module is a base module.
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author <mailto:info@18thdigitech.com>
+ * @copyright Copyright (c) 2023 18th DigiTech (https://www.18thdigitech.com)
+ * @package Eighteentech_Base
+ * @link https://www.18thdigitech.com/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Eighteentech_Base',
+    __DIR__
+);
