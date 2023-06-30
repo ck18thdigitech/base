49e38f9f292f7f3a04f316d027f6e05654208c0a
inderjeet18th <inderjeet@18thdigitech.net>
2023-06-30 18:13:05 +0530

    [Feature] Base module code check with coding standard [wip]

--- registration.php
+++ registration.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @author <mailto:info@18thdigitech.com>
+ * @author    <mailto:info@18thdigitech.com>
  * @copyright Copyright (c) 2023 18th DigiTech (https://www.18thdigitech.com)
- * @package Eighteentech_Base
- * @link https://www.18thdigitech.com/
+ * @package   Eighteentech_Base
+ * @link      https://www.18thdigitech.com/
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
